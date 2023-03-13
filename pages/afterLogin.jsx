94e92b4eebce8de03b7ed74ff7bc016491922586
sulphurninja <sulphurninja@gmail.com>
2023-03-14 02:24:41 +0530

    first commit

---
+++ pages/afterLogin.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Easylogout from '../components/Easylogout'
+
+function afterLogin() {
+    return (
+        <body>
+            <div >
+                <Easylogout />
+            </div>
+
+        </body>
+
+    )
+}
+
+export default afterLogin
