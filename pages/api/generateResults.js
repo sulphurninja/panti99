16da239420bb6fc4927ad36d36899f7dc92011f1
sulphurninja <sulphurninja@gmail.com>
2023-03-16 02:13:07 +0530

    final

--- pages/api/generateResults.js
+++ pages/api/generateResults.js
@@ -1,3 +1,4 @@
+import { MongoClient } from 'mongodb';
 import fs from 'fs';
 import path from 'path';
 
@@ -16,7 +17,7 @@ export default async function handler(req, res) {
     }
   }
 
-  // Generate and write the documents to results.json
+  // Generate the documents
   const docs = [];
   for (let i = 0; i < numDraws; i++) {
     const doc = {
@@ -26,16 +27,24 @@ export default async function handler(req, res) {
     docs.push(doc);
   }
 
-  const jsonData = JSON.stringify(docs, null, 2);
-
-  const resultsPath = path.join(process.cwd(), 'data', 'results.json');
+  // Connect to the database
+  const client = await MongoClient.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = client.db('test');
 
   try {
-    fs.writeFileSync(resultsPath, jsonData, 'utf-8');
-    console.log('Results written to file successfully');
-    res.status(200).json({ message: 'Results written to file successfully' });
+    // Insert the documents into the fetchResults collection
+    const collection = db.collection('fetchResults');
+    const result = await collection.insertMany(docs);
+    console.log(`Inserted ${result.insertedCount} documents into the fetchResults collection`);
+    res.status(200).json({ message: `Inserted ${result.insertedCount} documents into the fetchResults collection` });
   } catch (err) {
-    console.log('Error writing to file:', err);
-    res.status(500).json({ message: 'Error writing to file' });
+    console.error(err);
+    res.status(500).json({ message: 'Error inserting documents into the fetchResults collection' });
+  } finally {
+    // Close the connection after use
+    client.close();
   }
 }
